50fcb3c84f634dc10f5071c47a5524e769c2d7e0
Lexical Scoped <LexicalScoped@Gmail.com>
2022-10-23 15:48:43 -0500

    Part 2 of object oriented conversion for Youtube snake game tutorial series.

--- SnakeGame.py
+++ SnakeGame.py
@@ -20,9 +20,9 @@ Config = {
 }
 
 class Window():
-    def __init__(self):
-        self.X = Config["ScreenX"]
-        self.Y = Config["ScreenY"]
+    def __init__(self, ScreenX = 800, ScreenY = 600):
+        self.X = ScreenX
+        self.Y = ScreenY
         self.Title = "Lexical's Snake Game"
         self.Screen = pygame.display.set_mode([self.X, self.Y])
         pygame.display.set_caption(self.Title)
@@ -52,38 +52,28 @@ class Window():
     def Quit(self):
         sys.exit()
 
-Snake = {
-    "X": Config["ScreenX"] / 2,
-    "Y": Config["ScreenY"] / 2,
-    "Direction": "none",
-    "Color": Colors["Red"],
-    "Length": 0,
-    "Tail": [],
-}
+class Snake():
+    def __init__(self, ScreenX, ScreenY):
+        self.X = ScreenX / 2
+        self.Y = ScreenY / 2
+        self.Direction = "none"
+        self.Color = Colors["Red"]
+        self.Length = 0
+        self.Tail = []
 
-def ResetSnake():
-    Snake["X"] = Config["ScreenX"] / 2
-    Snake["Y"] = Config["ScreenY"] / 2
-    Snake["Direction"] = "none"
-    Snake["Length"] = 0
-    Snake["Tail"] = []
+class Food():
+    def __init__(self, ScreenX, ScreenY):
+        self.X = round(random.randrange(0,ScreenX - Config["BlockSize"]), -1)
+        self.Y = round(random.randrange(0,ScreenY - Config["BlockSize"]), -1)
+        self.Color = Colors["LightBlue"]
 
-Food = {
-    "X": 0,
-    "Y": 0,
-    "Color": Colors["LightBlue"],
-}
 
-def RandomizeFoodlocation():
-    Food["X"] = round(random.randrange(0,Config["ScreenX"] - Config["BlockSize"]), -1)
-    Food["Y"] = round(random.randrange(0,Config["ScreenY"] - Config["BlockSize"]), -1)
-
-def DrawGame(screen):
+def DrawGame(screen, snake, food):
     screen.fill(Config["background"])
-    pygame.draw.rect(screen, Snake["Color"], [ Snake["X"], Snake["Y"], Config["BlockSize"], Config["BlockSize"]])
-    for tail in Snake["Tail"]:
-        pygame.draw.rect(screen, Snake["Color"], [ tail[0], tail[1], Config["BlockSize"], Config["BlockSize"]])
-    pygame.draw.rect(screen, Food["Color"], [ Food["X"], Food["Y"], Config["BlockSize"], Config["BlockSize"]])
+    pygame.draw.rect(screen, snake.Color, [ snake.X, snake.Y, Config["BlockSize"], Config["BlockSize"]])
+    for tail in snake.Tail:
+        pygame.draw.rect(screen, snake.Color, [ tail[0], tail[1], Config["BlockSize"], Config["BlockSize"]])
+    pygame.draw.rect(screen, food.Color, [ food.X, food.Y, Config["BlockSize"], Config["BlockSize"]])
     pygame.display.update()
 
 def main():
@@ -100,8 +90,8 @@ def main():
                     display.Quit()
                 if event.key == pygame.K_n:
                     bGame = True
-                    ResetSnake()
-                    RandomizeFoodlocation()
+                    snake = Snake(display.X, display.Y)
+                    food = Food(display.X, display.Y)
                     
         display.MainMenu()
 
@@ -111,44 +101,49 @@ def main():
                     display.Quit()
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                        if Snake["Direction"] != "right":
-                            Snake["Direction"] = "left"
+                        if snake.Direction != "right":
+                            snake.Direction = "left"
                     if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                        if Snake["Direction"] != "left":
-                            Snake["Direction"] = "right"
+                        if snake.Direction != "left":
+                            snake.Direction = "right"
                     if event.key == pygame.K_UP or event.key == pygame.K_w:
-                        if Snake["Direction"] != "down":
-                            Snake["Direction"] = "up"
+                        if snake.Direction != "down":
+                            snake.Direction = "up"
                     if event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                        if Snake["Direction"] != "up":
-                            Snake["Direction"] = "down"   
+                        if snake.Direction != "up":
+                            snake.Direction = "down"   
 
-            if Snake["Direction"] == "left":
-                Snake["X"] -= Config["BlockSize"]
-            if Snake["Direction"] == "right":
-                Snake["X"] += Config["BlockSize"]
-            if Snake["Direction"] == "up":
-                Snake["Y"] -= Config["BlockSize"]
-            if Snake["Direction"] == "down":
-                Snake["Y"] += Config["BlockSize"]
+            if snake.Direction == "left":
+                snake.X -= Config["BlockSize"]
+            if snake.Direction == "right":
+                snake.X += Config["BlockSize"]
+            if snake.Direction == "up":
+                snake.Y -= Config["BlockSize"]
+            if snake.Direction == "down":
+                snake.Y += Config["BlockSize"]
                         
-            DrawGame(display.Screen)
+            DrawGame(display.Screen, snake, food)
 
-            if Snake["X"] < 0 or Snake["X"] >= Config["ScreenX"] or Snake["Y"] < 0 or Snake["Y"] >= Config["ScreenY"]:
+            if snake.X < 0 or snake.X >= Config["ScreenX"] or snake.Y < 0 or snake.Y >= Config["ScreenY"]:
                 print("you hit a wall")
                 bGame = False
 
-            if [Snake["X"], Snake["Y"]] in Snake["Tail"]:
+            if [snake.X, snake.Y] in snake.Tail:
                 print("you hit your own tail")
                 bGame = False
 
-            if Snake["X"] == Food["X"] and Snake["Y"] == Food["Y"]:
-                Snake["Length"] += 1
-                RandomizeFoodlocation()
+            if snake.X == food.X and snake.Y == food.Y:
+                snake.Length += 1
+                del food
+                food = Food(display.X, display.Y)
 
-            Snake["Tail"].append([Snake["X"], Snake["Y"]])
-            if len(Snake["Tail"]) > Snake["Length"]:
-                del Snake["Tail"][0]
+            snake.Tail.append([snake.X, snake.Y])
+            if len(snake.Tail) > snake.Length:
+                del snake.Tail[0]
+
+            if not bGame:
+                del food
+                del snake
 
             clock.tick(Config["Speed"])
 
